4d1ff4894a3a226b3d1c4d25fef9a3af7c20f406
HanshangZhu <zcabhz4@ucl.ac.uk>
2025-05-28 01:37:24 +0100

    Robotic Manipulation Test_prompt

--- run_paligemma2.py
+++ run_paligemma2.py
@@ -29,21 +29,28 @@ model = PaliGemmaForConditionalGeneration.from_pretrained(
 model.eval()
 
 # Load and resize image
-image_path = "/home/hz/Pictures/Screenshots/Screenshot from 2025-05-27 13-17-44.png"
+image_path = "/home/hz/Pictures/Screenshots/Screenshot from 2025-05-28 01-25-51.png"
 image = Image.open(image_path).convert("RGB")
 img_resized = image.resize((224, 224))  # Optional for visualization
 img_resized.show()  # Display the image (optional)
 
+################################################################################
 # Prepare multimodal prompt with <image> token
-prompt = "<image> What programming language is this code written in?"
+prompt = '''<image> Produce a chain of thought reasoning for the following image and answer the question:
+Given your end effector can hold the eraser, and you can move it in any direction.
+Given the image, if you are a robotic arm, how would you erase the whiteboard?
+identify the steps you would take to accomplish this task, considering the tools and actions available to you.
 
+'''
+######################################################################
 
 # Preprocess inputs
 inputs = processor(images=image, text=prompt, return_tensors="pt").to(model.device)
+print(inputs.keys) # check input keys
 
 # Generate output
 with torch.no_grad():
-    output_ids = model.generate(**inputs, max_new_tokens=64)
+    output_ids = model.generate(**inputs, max_new_tokens=2000, temperature=0)
 
 # Decode result
 caption = processor.batch_decode(output_ids, skip_special_tokens=True)[0]
